d64bfe8431c9c34a68a673844e044c46837bcb54
shanzm <2351144690@qq.com>
2020-03-19 02:56:17 +0800

    2020年3月19日 02:55:43- Update：关于其他非接口实现类的注册

--- 010MVC-AutoFac/TestMVC/Controllers/HomeController.cs
+++ 010MVC-AutoFac/TestMVC/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using TestIService;
+using TestMVC.Common;
 
 //PM> Install-Package AutoFac.Mvc5
 
@@ -13,17 +14,37 @@ namespace TestMVC.Controllers
     {
 
         public IUserService userService { get; set; }//通过AutoFac自动为我们赋值一个IUserService接口实现对象
-        // GET: Home
+
+        //测试：控制器（也就是IController接口的实现类）中的接口类型的属性的注入
         public ActionResult CheckLogin()
         {
             bool b = userService.CheckLogin("shanzm", "123456");
             return Content(b.ToString());//结果：页面显示true
         }
 
+
+        //测试：接口实现类中的接口类型的属性的注入
         public ActionResult UserAddNews()
         {
             string result = userService.UserAction("shanzm");
             return Content(result);//结果：页面显示：shanzm 添加新闻 ：Title：2020年3月16日-新冠病毒,Content：中国境内的新冠病毒被有效遏制
         }
+
+        //测试：未实现任何接口的静态类中的接口属性的注入
+        public ActionResult TestHelper()
+        {
+            string result = Helper.Test();
+            return Content(result);//结果：页面显示：shanzm 添加新闻 ：Title：2020年3月16日-新冠病毒,Content：中国境内的新冠病毒被有效遏制
+        }
+
+        //测试:未实现任何接口的非静态类的注册即其接口类型的属性的注入
+        public ActionResult TestPerson()
+        {
+            Person p = DependencyResolver.Current.GetService<Person>();
+            string result = p.Test();
+            return Content(result);//结果： 页面显示：shanzm 添加新闻 ：Title：2020年3月16日-新冠病毒,Content：中国境内的新冠病毒被有效遏制
+        }
+
+        //Undone：关于Quartz定时任务使用AutoFac注入
     }
 }
