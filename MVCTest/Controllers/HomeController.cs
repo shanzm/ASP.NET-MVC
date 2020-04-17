f55a8ea514d3d57f2a660b0af93a81b33028ce64
shanzm <2351144690@qq.com>
2020-04-17 23:16:22 +0800

    2020年4月17日 23:15:35 -Create：MVCTest
    该项目为MVC项目，用于测试架构和WebAPI

---
+++ MVCTest/Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Bll;
+using Dto;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+//该MVC项目是用于测试WebAPI项目的
+
+namespace MVCTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        PersonBll pBll = new PersonBll();
+        // GET: Home
+        public ActionResult Index()
+        {
+            PersonDto pDto = pBll.GetPerson(1);
+            return Json(pDto,JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ MVCTest/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCTest.MvcApplication" Language="C#" %>
