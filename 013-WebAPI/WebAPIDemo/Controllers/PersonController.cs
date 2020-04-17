411da73a7580314aaaf33fed28cdf30274ff6600
shanzm <2351144690@qq.com>
2020-04-17 21:40:41 +0800

    2020年4月17日 21:40:15- Create：WebAPIDemo
    测试第一个WebAPI项目

---
+++ 013-WebAPI/WebAPIDemo/Controllers/PersonController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace WebAPIDemo.Controllers
+{
+    public class PersonController : ApiController
+    {
+        // GET: api/Person
+        public IEnumerable<string> Get()//TEST:http://localhost:62814/api/person
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/Person/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Person
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Person/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Person/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ 013-WebAPI/WebAPIDemo/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebAPIDemo.WebApiApplication" Language="C#" %>
