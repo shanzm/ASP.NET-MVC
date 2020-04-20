8b77b17b98e7efdfb13a2a5c028406ef670f05cd
shanzm <2351144690@qq.com>
2020-04-20 23:26:23 +0800

    2020年4月20日 23:24:54- Create：创建测试WebAPI的项目
    一个是使用HttpClient发送请求的控制台项目，一个是ＭＶＣ项目

---
+++ 013-WebAPI/测试WebAPI项目/HttpClientTestWebAPI/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HttpClientTestWebAPI
+{
+    class Program
+    {
+
+        //这里使用HttpClient对象发送请求
+        //注意运行前，先要把WebAPI项目运行
+        //调试方法：开多个VS，一个VS运行WebAPI项目，一个VS运行该测试项目
+        static void Main(string[] args)
+        {
+            HttpClient hc = new HttpClient();
+            string html = hc.GetStringAsync("http://localhost:62814/api/person/2").Result;
+            Console.WriteLine(html); ;
+            Console.ReadKey();
+        }
+
+    }
+}
