5171c6fc07e21a68328896ae2763a6c4668038cb
shanzm <2351144690@qq.com>
2020-05-02 03:54:05 +0800

    2020年5月2日 03:53:39- Update：全局过滤和特定Action或Controller过滤

--- 004FilterTest1/Filters/ResultFilterAttribute.cs
+++ 004FilterTest1/Filters/ResultFilterAttribute.cs
@@ -7,6 +7,12 @@ using System.Web.Mvc;
 
 namespace _04FilterTest1.Filters
 {
+    //注意定义过滤器就是实现MVC5个过滤器接口中的某一个
+    //然后在Global.asax.cs文件中注意该过滤器，实现全局过滤
+
+    //但是我们也可以在实现过滤器接口的同时，也继承FilterAttribute特性
+    //这样我们就可以把该过滤器当作特性标签标注在特定的action或controller上，而不是全局过滤
+
     ///这个结果过滤器是计算OnResultExecuting（）和OnResultExecuted(）之间的触发间隔
     public class ResultFilterAttribute : FilterAttribute, IResultFilter
     {
