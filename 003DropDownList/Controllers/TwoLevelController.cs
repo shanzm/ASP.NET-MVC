f77f2fe4e16e63320a97229c766e2976779aafb1
shanzm_aimu <2351144690@qq.com>
2020-06-03 11:05:38 +0800

    2020年6月3日 11:03:56- Update：实现了下拉列表的二级联动，
    根据第一个下拉列表的数据动态的拼接第二个下拉列表的选项数据

--- 003DropDownList/Controllers/TwoLevelController.cs
+++ 003DropDownList/Controllers/TwoLevelController.cs
@@ -8,23 +8,27 @@ using _02DropDownList.Models;
 namespace _02DropDownList.Controllers
 {
     public class TwoLevelController : Controller
-    {   
+    {
         public ActionResult Index()
         {
             ViewBag.Date = DateTime.Now.ToString();
             List<namelist> llNameList = buildNameList();
-            ViewData["SEX"] = llNameList.Select(i => new SelectListItem { Value = i.sex, Text = i.sex });
-            ViewData["NAME"] = llNameList.Select(i => new SelectListItem { Value = i.name, Text = i.name });
+            //ViewData["SEX"] = llNameList.Select(i => new SelectListItem { Value = i.sex, Text = i.sex });
+            ViewData["SEX"] = new List<SelectListItem>() { new SelectListItem { Value = "male", Text = "male" }, new SelectListItem { Value = "female", Text = "female" } };
+
+
+            //ViewData["NAME"] = llNameList.Select(i => new SelectListItem { Value = i.name, Text = i.name });
+            ViewData["NAME"] = new List<SelectListItem>() { new SelectListItem { Value = "000", Text = "请先选择性别" } };
             return View();
 
         }
 
-        public JsonResult GetName(string id)   //view界面选择sex的，输出对应的name
+        public JsonResult GetName(string sex)   //view界面选择sex的，输出对应的name
         {
             List<namelist> llNameList = new List<namelist>();
             llNameList = buildNameList();
             namelist ccnamelist = new namelist();
-            var varNameList = llNameList.Where(i => i.sex == id).Select(i => i.name);
+            var varNameList = llNameList.Where(i => i.sex == sex).Select(i => i.name);
             return Json(varNameList.ToList(), JsonRequestBehavior.AllowGet);
         }
 
@@ -36,7 +40,7 @@ namespace _02DropDownList.Controllers
                 namelist ccnamelist = new namelist();
                 ccnamelist.index = _count;
                 ccnamelist.sex = "male";
-                ccnamelist.name = "Tommy " + _count.ToString();
+                ccnamelist.name = "男孩 " + _count.ToString();
                 llnamelist.Add(ccnamelist);
             }
             for (int _count = 0; _count < 4; _count++)
@@ -44,7 +48,7 @@ namespace _02DropDownList.Controllers
                 namelist ccnamelist = new namelist();
                 ccnamelist.index = _count;
                 ccnamelist.sex = "female";
-                ccnamelist.name = "Kitty " + _count.ToString();
+                ccnamelist.name = "女孩 " + _count.ToString();
                 llnamelist.Add(ccnamelist);
             }
             return llnamelist;
