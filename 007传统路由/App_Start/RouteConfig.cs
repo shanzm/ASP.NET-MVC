d5ccc8c8db569d6adc28746084edc3f07c5c4741
shanzm_aimu <2351144690@qq.com>
2020-08-12 08:03:31 +0800

    2020年8月12日 08:03:06- Update：添加路由规则

--- 007传统路由/App_Start/RouteConfig.cs
+++ 007传统路由/App_Start/RouteConfig.cs
@@ -38,6 +38,7 @@ namespace _007传统路由
             //则当我输入“http://localhost:65034/welcome”就会出错，因为它也能与Default路由匹配，
             //但是只是符合Default路由的模式，并不能指向正确的action
             //按照Default路由匹配，它实际就是：http://localhost:65034/welcome/index  而实际是没有这个URL的
+            //简而言之，路由匹配是自上而下的，匹配到第一个满足的即技术
             #endregion
 
             #region 路由约束
@@ -45,8 +46,14 @@ namespace _007传统路由
             //在特性路由中我们使用类似于{ id:int}的格式约束路由的某一个参数的类型
             //也可以使用正则表达式｛year:regex{^\d{4}$}
             //传统路由使用一个单独的参数,且传统路由的约束在使用正则表达式的时候，可以省略"^""$"的位置匹配
+
+            //这里注意：直接调试输入路由：localhost://65043/2020/08/11 会报错，因为该url首先匹配的是上面的control/action/id，所以会报错，因为我们就没有名称为2020的控制器
+            //所有需要调试的话，可以先把上面的Default路由注释掉
             #endregion
             routes.MapRoute("blogs", "{year}/{month}/{day}", new { controller = "blog", action = "index" }, new { year = @"\d{4}", month = @"\d{2}", day = @"\d{2}" });
+
+
+            routes.MapRoute("timeRoute", "{controller}/{action}_{year}_{month}_{day}", new { controller = "home", action = "time" }, new { year = @"\d{4}" });
 
             ///自定义路由约束
             ///约束一个路由只响应Get请求
---
+++ 007传统路由/Controllers/BlogController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Contexts;
+using System.Web;
+using System.Web.Mvc;
+
+namespace _007传统路由.Controllers
+{
+    public class BlogController : Controller
+    {
+        // GET: Blog
+        public ActionResult Index()
+        {
+            return Content("路由：localhost:65034/2020/08/11");
+        }
+    }
+}
