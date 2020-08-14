4f8959d0887fe2ca87ba6680746edc2bd8cf33dc
shanzm_aimu <2351144690@qq.com>
2020-08-14 18:24:37 +0800

    2020年8月14日 18:24:24- Update：添加注释

---
+++ 016zTree/Controllers/TestJsonController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using _016zTree.Models;
+
+namespace _016zTree.Controllers
+{
+    public class TestJsonController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult GetTree()
+        {
+            List<Employee> Employees1 = new List<Employee>()
+            {
+                new  Employee(){EmpNo=001,Name="开发张三"},
+                new Employee (){EmpNo=002,Name="开发李四"},
+                new Employee (){EmpNo=003,Name="开发王五"},
+                new Employee (){EmpNo=004,Name="开发赵六"},
+                new Employee (){EmpNo=005,Name="开发周七"}
+            };
+
+            List<Employee> Employees2 = new List<Employee>()
+            {
+                new  Employee(){EmpNo=006,Name="项目张三"},
+                new Employee (){EmpNo=007,Name="项目李四"},
+                new Employee (){EmpNo=008,Name="项目王五"},
+                new Employee (){EmpNo=009,Name="项目赵六"}
+
+            };
+
+            Department Development1 = new Department() { DepNo = 0001, Name = "开发", Employees = Employees1 };
+            Department Development2 = new Department() { DepNo = 0002, Name = "项目", Employees = Employees2 };
+
+            List<Department> Departments = new List<Department>() { Development1, Development2 };
+
+            Company Company = new Company() { Id = 00001, Name = "公司", Departments = Departments };
+
+            return new JsonNetResult() { Data = Company };
+
+        }
+    }
+}
---
+++ 016zTree/Models/EmpTree.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace _016zTree.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<Department> Departments { get; set; }
+    }
+
+    public class Department
+    {
+        public int DepNo { get; set; }
+        public string Name { get; set; }
+        public List<Employee> Employees { get; set; }
+    }
+
+    public class Employee
+    {
+        public int EmpNo { get; set; }
+        public string Name { get; set; }
+    }
+}
